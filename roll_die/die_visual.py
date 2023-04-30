ddd3399f99aa20a10fcefd204080df4e31642576
Julien Briel <julien_briel@hotmail.com>
2023-04-30 19:42:30 -0400

    ReadMe of Readme's

--- roll_die/die_visual.py
+++ roll_die/die_visual.py
@@ -3,7 +3,7 @@ from plotly.graph_objects import Bar, Layout
 from plotly import offline
 
 die_1 = Die()
-die_2 = Die(10)
+die_2 = Die(6)
 
 
 results = [die_1.roll() + die_2.roll() for roll_num in range(10000)]
@@ -20,6 +20,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Result of rolling two D6 Die 1000 times',
+my_layout = Layout(title='Result of rolling two D6 Die 10000 times',
                       xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
