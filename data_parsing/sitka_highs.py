40acebe00c50f58317b9c7a065ae4fe37c55c553
Julien Briel <julien_briel@hotmail.com>
2023-04-29 20:28:22 -0400

    Plot Sitka high temperatures in 2018

--- data_parsing/sitka_highs.py
+++ data_parsing/sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 filename = 'data\\sitka_weather_07-2018_simple.csv'
 
@@ -6,5 +7,21 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, colum_header in enumerate(header_row):
-        print(index, colum_header)
+    # Get high temperatures from file
+
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+# Plot the high temperatures
+plt.style.use('seaborn-v0_8-darkgrid')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot
+ax.set_title("Daily High temperatures, July 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
